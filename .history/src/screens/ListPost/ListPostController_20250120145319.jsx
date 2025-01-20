045c8fa47163d20f76876ac9a2645339efffa170
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-01-20 14:54:18 +0300

    Add Navigator Packages

---
+++ .history/src/screens/ListPost/ListPostController_20250120145319.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ListPostController = () => {
+  return (
+    <View>
+      <Text>ListPostController</Text>
+    </View>
+  );
+};
+
+export default ListPostController;
+
+const styles = StyleSheet.create({});
---
+++ .history/src/screens/ListPost/ListPostView_20250120145316.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ListPostView = () => {
+  return (
+    <View>
+      <Text>ListPostView</Text>
+    </View>
+  );
+};
+
+export default ListPostView;
+
+const styles = StyleSheet.create({});
